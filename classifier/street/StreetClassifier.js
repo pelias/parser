16a24320083e60a06428c802eb21d27de136a705
missinglink <insomnia@rcpt.at>
2019-03-29 08:58:24 +0100

    classifier: tweaks

--- classifier/street/StreetClassifier.js
+++ classifier/street/StreetClassifier.js
@@ -2,27 +2,29 @@ const fs = require('fs')
 const path = require('path')
 const WordClassifier = require('../../classification/WordClassifier')
 const Classification = require('../../classification/Classification')
+const dictPath = path.join(__dirname, `../../resources/libpostal/dictionaries`)
 
 // dictionaries sourced from the libpostal project
 // see: https://github.com/openvenues/libpostal
 
-const whitelist = [
-  { lang: 'en' },
-  { lang: 'de' },
-  { lang: 'fr' }
-]
+// include all languages
+const whitelist = fs.readdirSync( dictPath ).filter( p => !p.includes('.') )
+
+// optionally control which languages are included
+// note: reducing the languages will have a considerable performance benefit
+// const whitelist = [ 'en', 'de', 'fr' ]
+
 class StreetClassifier extends WordClassifier {
   constructor() {
     super()
     this.loadStreetTypes()
+    this.loadSuffixes()
   }
 
   loadStreetTypes() {
     this.streetTypes = {}
-    this.suffixes = {}
-    
-    whitelist.forEach(item => {
-      let filepath = path.join( __dirname, `../../resources/libpostal/dictionaries/${item.lang}/street_types.txt` )
+    whitelist.forEach(lang => {
+      let filepath = path.join( dictPath, lang, 'street_types.txt' )
       if( !fs.existsSync( filepath ) ){ return }
       let dict = fs.readFileSync(filepath, 'utf8')
       dict.split('\n').forEach(row => {
@@ -31,9 +33,12 @@ class StreetClassifier extends WordClassifier {
         })
       }, this)
     }, this)
+  }
 
-    whitelist.forEach(item => {
-      let filepath = path.join(__dirname, `../../resources/libpostal/dictionaries/${item.lang}/concatenated_suffixes_separable.txt`)
+  loadSuffixes() {
+    this.suffixes = {}
+    whitelist.forEach(lang => {
+      let filepath = path.join( dictPath, lang, 'concatenated_suffixes_separable.txt' )
       if( !fs.existsSync( filepath ) ){ return }
       let dict = fs.readFileSync(filepath, 'utf8')
       dict.split('\n').forEach(row => {
@@ -53,12 +58,14 @@ class StreetClassifier extends WordClassifier {
     if( this.streetTypes.hasOwnProperty( body ) ) {
       if( body.length < 2 ){ confidence = 0.2 } // single letter streets are uncommon
       this.add( new Classification( span, 'STREET:SUFFIX', confidence ) )
+      return
     }
 
-    // try again for abbreviations denoted by a period such as 'str.'
+    // try again for abbreviations denoted by a period such as 'str.', also O(1)
     else if( body.slice(-1) === '.' && this.streetTypes.hasOwnProperty( body.slice( 0, -1 ) )){
       if( body.length < 3 ){ confidence = 0.2 } // single letter streets are uncommon
       this.add( new Classification( span, 'STREET:SUFFIX', confidence) )
+      return
     }
 
     // else use a slower suffix check which is O(n)
@@ -69,7 +76,7 @@ class StreetClassifier extends WordClassifier {
         if( span.body.length <= token.length ){ continue }
         if( body.slice( -token.length ) === token ){
           this.add( new Classification( span, 'STREET', confidence ) )
-          break
+          return
         }
       }
     }
