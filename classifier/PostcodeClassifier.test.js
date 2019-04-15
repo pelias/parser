524fe46d42a906fcaeb50e40295c26e38285096a
missinglink <insomnia@rcpt.at>
2019-04-15 17:54:07 +0200

    test: add PostcodeClassifier test

---
+++ classifier/PostcodeClassifier.test.js
@@ -0,0 +1,76 @@
+const PostcodeClassifier = require('./PostcodeClassifier')
+const PostcodeClassification = require('../classification/PostcodeClassification')
+const Span = require('../tokenization/Span')
+
+module.exports.tests = {}
+
+function classify (body) {
+  let c = new PostcodeClassifier()
+  let s = new Span(body)
+  c.each(s)
+  return s
+}
+
+module.exports.tests.contains_numerals = (test) => {
+  test('contains numerals: honours contains.numerals boolean', (t) => {
+    let c = new PostcodeClassifier()
+    let s = new Span('100')
+    s.contains.numerals = false
+    c.each(s)
+    t.deepEqual(s.classifications, {})
+    t.end()
+  })
+}
+
+module.exports.tests.classify = (test) => {
+  test('classify: USA ZIP', (t) => {
+    let s = classify('10010')
+    t.deepEqual(s.classifications, { PostcodeClassification: new PostcodeClassification(1.0) })
+    t.end()
+  })
+  test('classify: USA ZIP Plus 4', (t) => {
+    let s = classify('99577-0727')
+    t.deepEqual(s.classifications, { PostcodeClassification: new PostcodeClassification(1.0) })
+    t.end()
+  })
+  test('classify: DEU', (t) => {
+    let s = classify('10117')
+    t.deepEqual(s.classifications, { PostcodeClassification: new PostcodeClassification(1.0) })
+    t.end()
+  })
+  test('classify: NZD', (t) => {
+    let s = classify('6012')
+    t.deepEqual(s.classifications, { PostcodeClassification: new PostcodeClassification(1.0) })
+    t.end()
+  })
+  test('classify: AUD', (t) => {
+    let s = classify('2000')
+    t.deepEqual(s.classifications, { PostcodeClassification: new PostcodeClassification(1.0) })
+    t.end()
+  })
+  test('classify: FRA', (t) => {
+    let s = classify('75000')
+    t.deepEqual(s.classifications, { PostcodeClassification: new PostcodeClassification(1.0) })
+    t.end()
+  })
+  test('classify: JAP', (t) => {
+    let s = classify('100-0000')
+    t.deepEqual(s.classifications, { PostcodeClassification: new PostcodeClassification(1.0) })
+    t.end()
+  })
+  test('classify: RUS', (t) => {
+    let s = classify('101000')
+    t.deepEqual(s.classifications, { PostcodeClassification: new PostcodeClassification(1.0) })
+    t.end()
+  })
+}
+
+module.exports.all = (tape, common) => {
+  function test (name, testFunction) {
+    return tape(`PostcodeClassifier: ${name}`, testFunction)
+  }
+
+  for (var testCase in module.exports.tests) {
+    module.exports.tests[testCase](test, common)
+  }
+}
