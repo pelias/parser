e658d8407cfd1c83a0de5f2b885c8c24df5ad80f
missinglink <insomnia@rcpt.at>
2019-04-16 16:24:02 +0200

    solver: update SubsetFilter to not dedupe between solutions of the same score

--- solver/SubsetFilter.js
+++ solver/SubsetFilter.js
@@ -4,7 +4,14 @@ class SubsetFilter {
   solve (tokenizer) {
     tokenizer.solution = tokenizer.solution.filter((s, i) => {
       for (let j = 0; j < tokenizer.solution.length; j++) {
-        if (i === j) { return true } // do not check itself
+        // do not check itself
+        if (i === j) { return true }
+
+        // do not filter solutions with the same score
+        // do not favour solutions with lower scores (if for any reason they are not sorted)
+        if (tokenizer.solution[i].score <= s.score) { return false }
+
+        // if two solutions cover the same tokens, remove one of them
         if (tokenizer.solution[i].coversSameClassification(s)) { return false }
       }
       return true
