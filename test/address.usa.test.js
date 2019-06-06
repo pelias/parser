0ed71c534a289ea25e88f9ac4e5095714bd0806f
Peter Johnson <insomnia@rcpt.at>
2019-06-06 18:50:26 +0200

    feat: autocomplete jitter fixes (#44)
    * fix: remove unused scheme
    
    * fix: improve street_name scheme
    
    * feat(CompositeClassifier): improvements to composite classifier, see PR notes
    
    * feat(test): add autocomplete jitter test cases

--- test/address.usa.test.js
+++ test/address.usa.test.js
@@ -30,6 +30,24 @@ const testcase = (test, common) => {
 
   // postcode not allowed in first position otherwise
   assert('90210 Foo', [])
+
+  // autocomplete street name jitter
+  // note: we are only testing the street name stays the same throughout
+  assert('N FISKE AVE', [{ street: 'N FISKE AVE' }], true)
+  assert('N FISKE AVE P', [{ street: 'N FISKE AVE' }], true)
+  assert('N FISKE AVE Po', [{ street: 'N FISKE AVE' }, { region: 'Po' }], true)
+  assert('N FISKE AVE Por', [{ street: 'N FISKE AVE' }, { region: 'Por' }], true)
+  assert('N FISKE AVE Port', [{ street: 'N FISKE AVE' }, { locality: 'Port' }], true)
+  assert('N FISKE AVE Portl', [{ street: 'N FISKE AVE' }], true)
+  assert('N FISKE AVE Portla', [{ street: 'N FISKE AVE' }], true)
+  assert('N FISKE AVE Portlan', [{ street: 'N FISKE AVE' }], true)
+  assert('N FISKE AVE Portland', [{ street: 'N FISKE AVE' }, { locality: 'Portland' }], true)
+  assert('N DWIGHT AVE Portland O', [{ street: 'N DWIGHT AVE' }, { locality: 'Portland' }], true)
+  assert('N DWIGHT AVE Portland Or', [{ street: 'N DWIGHT AVE' }, { locality: 'Portland' }, { region: 'Or' }], true)
+  assert('N DWIGHT AVE Portland Ore', [{ street: 'N DWIGHT AVE' }, { locality: 'Portland' }], true)
+  assert('N DWIGHT AVE Portland Oreg', [{ street: 'N DWIGHT AVE' }, { locality: 'Portland' }], true)
+  assert('N DWIGHT AVE Portland Orego', [{ street: 'N DWIGHT AVE' }, { locality: 'Portland' }], true)
+  assert('N DWIGHT AVE Portland Oregon', [{ street: 'N DWIGHT AVE' }, { locality: 'Portland' }, { region: 'Oregon' }], true)
 }
 
 module.exports.all = (tape, common) => {
