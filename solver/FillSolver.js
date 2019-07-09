a9811b52f2b5db8c2bd27f0c3f5064344422cf31
Joxit <joxit972@gmail.com>
2019-07-09 14:24:25 +0200

    fix(FillSolver): don't alter spans
    Now I replace the solution with the correct phrase

--- solver/FillSolver.js
+++ solver/FillSolver.js
@@ -4,16 +4,22 @@ class FillSolver {
       // Get streets from this solution
       const streets = solution.pair.filter(p => p.classification.constructor.name === 'StreetClassification')
       // Get all nodes that are not in the solution
-      const missings = tokenizer.section[0].graph.edges.child.filter(c => !solution.pair.some(p => p.span.intersects(c)))
+      const missings = tokenizer.section.reduce((acc, section) => {
+        return acc.concat(section.graph.findAll('child').filter(c => !solution.pair.some(p => p.span.intersects(c))))
+      }, [])
 
+      // For all missing spans, check if they are street prefix and complete the solution
+      // The missing span should not be a end token
       missings.forEach(missing => {
         const street = streets.find(s => s.span.end === missing.start - 1)
         const prefix = street && street.span.graph.findOne('child:first')
-        if (prefix && prefix.classifications.StreetPrefixClassification) {
-          street.span.setBody(`${street.span.body} ${missing.body}`)
-          street.span.graph.add('child', missing)
-          street.span.graph.remove('child:last', street.span.graph.findOne('child:last'))
-          street.span.graph.add('child:last', missing)
+
+        if (prefix && prefix.classifications.StreetPrefixClassification && !missing.classifications.EndTokenClassification) {
+          const span = prefix.graph.findAll('parent').find(phrase => phrase.start === prefix.start && phrase.end === missing.end)
+          const streetIndex = solution.pair.indexOf(street)
+          if (span && streetIndex < solution.pair.length) {
+            solution.pair[streetIndex].span = span
+          }
         }
       })
     })
