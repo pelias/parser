a1fd6f0f83abde156be0e497e8b71dbd77c4874a
David Blackman <david@whizziwig.com>
2020-05-18 15:38:01 -0400

    optimize regexp matching

--- classifier/UnitClassifier.js
+++ classifier/UnitClassifier.js
@@ -1,25 +1,38 @@
 const WordClassifier = require('./super/WordClassifier')
 const UnitClassification = require('../classification/UnitClassification')
 
+const AllNumbersRegExp = /^#?\d+$/
+const SingleLetterRegExp = /^#?[A-Za-z]$/
+const NumbersThenLetterRegExp = /^#?\d+[A-Za-z]$/
+const LetterThenNumbersRegExp = /^#?[A-Za-z]\d+$/
+
+// based on https://stackoverflow.com/questions/9213237/combining-regular-expressions-in-javascript
+function combineRegExps (...args) {
+  var components = []
+
+  args.forEach((arg) => {
+    components = components.concat(arg._components || arg.source)
+  })
+
+  var combined = new RegExp('(?:' + components.join(')|(?:') + ')')
+  return combined
+}
+
+const combinedUnitRegexp = combineRegExps(
+  AllNumbersRegExp,
+  SingleLetterRegExp,
+  NumbersThenLetterRegExp,
+  LetterThenNumbersRegExp
+)
+
 class UnitClassifier extends WordClassifier {
   each (span) {
     const prev = span.graph.findOne('prev')
     const hasPrevUnitToken = prev && prev.classifications.hasOwnProperty('UnitTypeClassification')
 
-    // all numbers or a single letter need to follow a unit type to be a unit
-    // ex: suite a, apt 22
-    if (
-      hasPrevUnitToken && (
-        // all numbers
-        /^#?\d+$/.test(span.body) ||
-        // single letter
-        /^#?[A-Za-z]$/.test(span.body) ||
-        // numbers followed by letter
-        /^#?\d+[A-Za-z]$/.test(span.body) ||
-        // letter followed by numbers
-        /^#?[A-Za-z]\d+$/.test(span.body)
-      )
-    ) {
+    // If the previous token in a unit word, like apt or suite
+    // and this token is something like A2, 3b, 120, A, label it as a unit (number)
+    if (hasPrevUnitToken && combinedUnitRegexp.test(span.body)) {
       span.classify(new UnitClassification(1))
     }
   }
