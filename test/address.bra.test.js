0d6d9cb2cc67d98aa5cb8376867123546b33a2ff
missinglink <insomnia@rcpt.at>
2020-05-19 18:31:39 +0200

    fix(scheme): update person schema to allow middle names

---
+++ test/address.bra.test.js
@@ -0,0 +1,20 @@
+const testcase = (test, common) => {
+  let assert = common.assert(test)
+
+  assert('Rua Raul Leite Magalhães, 65, Tapiraí - SP, 18180-000, Brazil', [
+    { street: 'Rua Raul Leite Magalhães' },
+    { housenumber: '65' },
+    { locality: 'Tapiraí' },
+    { region: 'SP' },
+    { postcode: '18180' },
+    { country: 'Brazil' }
+  ])
+}
+
+module.exports.all = (tape, common) => {
+  function test (name, testFunction) {
+    return tape(`address BRA: ${name}`, testFunction)
+  }
+
+  testcase(test, common)
+}
