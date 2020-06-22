e7316bcaaa8b417339e668d0dcdcb2cf318654d2
David Blackman <david@whizziwig.com>
2020-06-22 10:15:04 -0400

    start

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,4 @@
-const util = require('util')
-const pretty = require('./pretty')
-const Tokenizer = require('../tokenization/Tokenizer')
-const AddressParser = require('../parser/AddressParser')
 const input = process.argv.slice(2).join(' ')
+const DebugParser = require('./pretty')
 
-// tokenizer
-var start = new Date()
-const t = new Tokenizer(input)
-let took = new Date() - start
-pretty.tokenizer(t, util.format('(%sms)', took))
-
-// parser
-const parser = new AddressParser()
-took = parser.classify(t)
-pretty.classifications(t, util.format('(%sms)', took))
-took = parser.solve(t)
-pretty.solutions(t, util.format('(%sms)', took))
+new DebugParser(process.stdout.write.bind(process.stdout)).parse(input)
--- bin/pretty.js
+++ bin/pretty.js
@@ -1,130 +1,169 @@
 const util = require('util')
 const chalk = require('chalk')
 
-function tokenizer (tokenizer, label) {
-  console.log()
-  console.log('='.repeat(64))
-  console.log('TOKENIZATION %s', label)
-  console.log('-'.repeat(64))
+const Tokenizer = require('../tokenization/Tokenizer')
+const AddressParser = require('../parser/AddressParser')
 
-  process.stdout.write('INPUT'.padEnd(32) + '➜  ')
-  console.log(chalk.bold(tokenizer.span.body))
-  spans('SECTIONS', tokenizer.section)
-
-  for (let i = 0; i < tokenizer.section.length; i++) {
-    spans(util.format('S%d TOKENS', i), tokenizer.section[i].graph.findAll('child'))
-  }
-
-  for (let i = 0; i < tokenizer.section.length; i++) {
-    spans(util.format('S%d PHRASES', i), tokenizer.section[i].graph.findAll('phrase'))
-  }
-
-  console.log()
-}
-
-function spans (title, s) {
-  process.stdout.write(title.padEnd(32) + '➜  ')
-  if (s.length === 0) {
-    console.log()
-  }
-  for (let i = 0; i < s.length; i++) {
-    process.stdout.write(
-      chalk.bgBlue.bold(util.format(' %s ', s[i].body)) +
-      chalk.bgWhite.bold.gray(util.format(' %d:%d ', s[i].start, s[i].end))
-    )
-    if (i === s.length - 1) {
-      console.log()
-    } else {
-      process.stdout.write(' ')
+class DebugParser {
+  constructor (outputFunction) {
+    this.write = outputFunction
+    this.writeLine = (...s) => {
+      if (s) {
+        s.forEach(outputFunction)
+      }
+      outputFunction('\n')
     }
   }
-}
 
-function wordClassifications (tokenizer) {
-  console.log('-'.repeat(64))
-  console.log('WORDS')
-  console.log('-'.repeat(64))
-
-  for (let i = 0; i < tokenizer.section.length; i++) {
-    let section = tokenizer.section[i]
-    let children = section.graph.findAll('child')
-    for (let j = 0; j < children.length; j++) {
-      let word = children[j]
-      let keys = Object.keys(word.classifications)
-      if (!keys.length) { continue }
-      process.stdout.write(word.body.padEnd(32) + '➜  ')
-      for (let k in word.classifications) {
-        let classification = word.classifications[k]
-        let block = chalk.bgGreen.bold(util.format(' %s ', classification.label))
-        block += chalk.bgWhite.bold.gray(util.format(' %s ', classification.confidence.toFixed(2)))
-        process.stdout.write(block)
-        if (k !== keys.slice(-1)) {
-          process.stdout.write(' ')
+  tokenizer (tokenizer, label) {
+    const spans = (title, s) => {
+      this.write(title.padEnd(32) + '➜  ')
+      if (s.length === 0) {
+        this.writeLine()
+      }
+      for (let i = 0; i < s.length; i++) {
+        this.write(
+          chalk.bgBlue.bold(util.format(' %s ', s[i].body)) +
+            chalk.bgWhite.bold.gray(util.format(' %d:%d ', s[i].start, s[i].end))
+        )
+        if (i === s.length - 1) {
+          this.writeLine()
+        } else {
+          this.write(' ')
         }
       }
-      console.log()
     }
+
+    this.writeLine()
+    this.writeLine('='.repeat(64))
+    this.writeLine(`TOKENIZATION ${label}`)
+    this.writeLine('-'.repeat(64))
+
+    this.write('INPUT'.padEnd(32) + '➜  ')
+    this.writeLine(chalk.bold(tokenizer.span.body))
+    spans('SECTIONS', tokenizer.section)
+
+    for (let i = 0; i < tokenizer.section.length; i++) {
+      spans(util.format('S%d TOKENS', i), tokenizer.section[i].graph.findAll('child'))
+    }
+
+    for (let i = 0; i < tokenizer.section.length; i++) {
+      spans(util.format('S%d PHRASES', i), tokenizer.section[i].graph.findAll('phrase'))
+    }
+
+    this.writeLine()
   }
 
-  console.log()
-}
+  wordClassifications (tokenizer) {
+    this.writeLine('-'.repeat(64))
+    this.writeLine('WORDS')
+    this.writeLine('-'.repeat(64))
 
-function phraseClassifications (tokenizer) {
-  console.log('-'.repeat(64))
-  console.log('PHRASES')
-  console.log('-'.repeat(64))
-
-  for (let i = 0; i < tokenizer.section.length; i++) {
-    let section = tokenizer.section[i]
-    let phrases = section.graph.findAll('phrase')
-    for (let j = 0; j < phrases.length; j++) {
-      let phrase = phrases[j]
-      let keys = Object.keys(phrase.classifications)
-      if (!keys.length) { continue }
-      process.stdout.write(phrase.body.padEnd(32) + '➜  ')
-      for (let k in phrase.classifications) {
-        let classification = phrase.classifications[k]
-        let block = chalk.bgRed.bold(util.format(' %s ', classification.label))
-        block += chalk.bgWhite.bold.gray(util.format(' %s ', classification.confidence.toFixed(2)))
-        process.stdout.write(block)
-        if (k !== keys.slice(-1)) {
-          process.stdout.write(' ')
+    for (let i = 0; i < tokenizer.section.length; i++) {
+      let section = tokenizer.section[i]
+      let children = section.graph.findAll('child')
+      for (let j = 0; j < children.length; j++) {
+        let word = children[j]
+        let keys = Object.keys(word.classifications)
+        if (!keys.length) {
+          continue
         }
+        this.write(word.body.padEnd(32) + '➜  ')
+        for (let k in word.classifications) {
+          let classification = word.classifications[k]
+          let block = chalk.bgGreen.bold(util.format(' %s ', classification.label))
+          block += chalk.bgWhite.bold.gray(
+            util.format(' %s ', classification.confidence.toFixed(2))
+          )
+          this.write(block)
+          if (k !== keys.slice(-1)) {
+            this.write(' ')
+          }
+        }
+        this.writeLine()
       }
-      console.log()
     }
+
+    this.writeLine()
   }
 
-  console.log()
-}
+  phraseClassifications (tokenizer) {
+    this.writeLine('-'.repeat(64))
+    this.writeLine('PHRASES')
+    this.writeLine('-'.repeat(64))
 
-function classifications (tokenizer, label) {
-  console.log('='.repeat(64))
-  console.log('CLASSIFICATIONS %s', label)
-
-  wordClassifications(tokenizer, label)
-  phraseClassifications(tokenizer, label)
-}
-
-function solutions (tokenizer, label) {
-  console.log('='.repeat(64))
-  console.log('SOLUTIONS %s', label)
-  console.log('-'.repeat(64))
-
-  // print all solutions
-  tokenizer.solution.forEach(s => {
-    let score = chalk.yellow.bold('(' + s.score.toFixed(2) + ')')
-    console.log(score, '➜', s.pair.map(c => {
-      return {
-        [c.classification.label]: c.span.body
-        // offset: c.span.start
+    for (let i = 0; i < tokenizer.section.length; i++) {
+      let section = tokenizer.section[i]
+      let phrases = section.graph.findAll('phrase')
+      for (let j = 0; j < phrases.length; j++) {
+        let phrase = phrases[j]
+        let keys = Object.keys(phrase.classifications)
+        if (!keys.length) {
+          continue
+        }
+        this.write(phrase.body.padEnd(32) + '➜  ')
+        for (let k in phrase.classifications) {
+          let classification = phrase.classifications[k]
+          let block = chalk.bgRed.bold(util.format(' %s ', classification.label))
+          block += chalk.bgWhite.bold.gray(
+            util.format(' %s ', classification.confidence.toFixed(2))
+          )
+          this.write(block)
+          if (k !== keys.slice(-1)) {
+            this.write(' ')
+          }
+        }
+        this.writeLine()
       }
-    }))
-    console.log()
-  })
+    }
+
+    this.writeLine()
+  }
+
+  classifications (tokenizer, label) {
+    this.writeLine('='.repeat(64))
+    this.writeLine(`CLASSIFICATIONS ${label}`)
+
+    this.wordClassifications(tokenizer, label)
+    this.phraseClassifications(tokenizer, label)
+  }
+
+  solutions (tokenizer, label) {
+    this.writeLine('='.repeat(64))
+    this.writeLine(`SOLUTIONS ${label}`)
+    this.writeLine('-'.repeat(64))
+
+    // print all solutions
+    tokenizer.solution.forEach((s) => {
+      let score = chalk.yellow.bold('(' + s.score.toFixed(2) + ')')
+      this.writeLine(
+        score,
+        '➜',
+        s.pair.map((c) => {
+          return {
+            [c.classification.label]: c.span.body
+            // offset: c.span.start
+          }
+        })
+      )
+      this.writeLine()
+    })
+  }
+
+  parse (input) {
+    // tokenizer
+    var start = new Date()
+    const t = new Tokenizer(input)
+    let took = new Date() - start
+    this.tokenizer(t, util.format('(%sms)', took))
+
+    // parser
+    const parser = new AddressParser()
+    took = parser.classify(t)
+    this.classifications(t, util.format('(%sms)', took))
+    took = parser.solve(t)
+    this.solutions(t, util.format('(%sms)', took))
+  }
 }
 
-module.exports.tokenizer = tokenizer
-module.exports.spans = spans
-module.exports.classifications = classifications
-module.exports.solutions = solutions
+module.exports = DebugParser
