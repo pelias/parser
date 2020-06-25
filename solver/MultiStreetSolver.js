80643c549d8d7efe8c6f0470349f572101a2b182
missinglink <insomnia@rcpt.at>
2020-06-25 14:53:49 +0200

    feat(intersection_improvements): refactor code for clarity, add code comments

--- solver/MultiStreetSolver.js
+++ solver/MultiStreetSolver.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const HashMapSolver = require('./super/HashMapSolver')
 
 // classifications which are more granular than StreetClassification
@@ -12,6 +13,25 @@ const MORE_GRANULAR_THAN_STREET = [
 /**
  * If a 'multistreet' classification was detected then
  * add a new solution which covers all streets included.
+ *
+ * The ExclusiveCartesianSolver ensures that each public classification
+ * can exist only once per solution (ie. we can't have two postalcodes).
+ *
+ * Intersections are the only exception to this rule, so rather than
+ * modifying the ExclusiveCartesianSolver we use this solver to create
+ * the missing intersection solutions (those with 2x 'street' labels).
+ *
+ * One of the challenges is that there could be many different interpretations
+ * of the admin tokens, so we need to ensure that each of those permutations
+ * is also represented by a distinct intersection solution.
+ *
+ * The solver works by iterating over existing solutions looking for any
+ * which identified a street, it then clones that solution, removes any tokens
+ * less granular than street and attempts to add the new street token in its
+ * place.
+ *
+ * Care is taken to ensure that the resulting solution does not contain
+ * tokens in overlapping positions.
  */
 
 class MultiStreetSolver extends HashMapSolver {
@@ -19,43 +39,63 @@ class MultiStreetSolver extends HashMapSolver {
     let map = this.generateHashMap(tokenizer, true)
 
     // sanity checking
-    if (!map.hasOwnProperty('multistreet') || map.multistreet.pair.length < 1) { return }
-    if (!map.hasOwnProperty('street') || map.street.pair.length < 2) { return }
+    if (_.get(map, 'multistreet.pair.length', 0) < 1) { return }
+    if (_.get(map, 'street.pair.length', 0) < 2) { return }
 
     // only currently consider one multistreet parse (for simplicity)
     // @todo: there may be some rare cases where we detect more than one?
-    const multi = map.multistreet.pair[0]
+    const multi = _.first(map.multistreet.pair)
 
     // generate a list of streets which intersect the multistreet
     const streets = map.street.pair.filter(s => s.span.intersects(multi.span))
     if (streets.length < 2) { return }
 
-    // iterate over existing solutionss
-    tokenizer.solution.filter(solution => {
-      // solution must contain a street and also that street must intersect
-      // the multistreet classification.
+    // generate a list of candidate solutions which could potentially be
+    // cloned to generate new intersection solutions
+    let candidates = tokenizer.solution.filter(solution => {
+      // candidate solution must contain a street and also that street
+      // must intersect the multistreet classification.
       return solution.pair.some(s => (
         s.classification.constructor.name === 'StreetClassification' &&
         s.span.intersects(multi.span)
       ))
-    }).forEach(solution => {
-      // find the street solution pair
+    })
+
+    // truncate the candidates by making a copy of the current solution and removing all solution
+    // pairs which came before the street and also any pairs less granular than street
+    // (such as venue, housenumber etc.)
+    candidates = candidates.map(solution => {
+      // find the street solution pair (there should be exactly one)
       const street = solution.pair.find(s => s.classification.constructor.name === 'StreetClassification')
 
-      // make a copy of the current solution and remove all solution pairs which came before
-      // the street and also any pairs less granular than street (such as venue, housenumber etc.)
+      // remove some pairs from the solution
       const truncated = solution.copy()
       truncated.pair = truncated.pair.filter(s => (
         (s.span.start >= street.span.start) &&
         !MORE_GRANULAR_THAN_STREET.includes(s.classification.constructor.name)
       ))
 
-      // find all street classsification which intersect the 'multistreet' span
+      return truncated
+    })
+
+    // the truncation step above can generate duplicate solutions so a 'content hash'
+    // is generated in order to deduplicate them.
+    // note: this is purely a performance optimization as it generates fewer candidates
+    candidates = _.uniqBy(candidates, truncated => {
+      return truncated.pair.map(p => `${p.classification.label}:${p.span.norm}`).join('_')
+    })
+
+    // iterate over candidates and generate new intersection solutions
+    candidates.forEach(truncated => {
+      // find all street classsifications which intersect the 'multistreet' span
       // and also do not overlap an existing pair in this solution.
-      streets.forEach(s => {
-        if (truncated.pair.every(p => !p.span.intersects(s.span))) {
+      streets.forEach(street => {
+        if (truncated.pair.every(p => !p.span.intersects(street.span))) {
+          // make a copy of the truncated solution and add the additional street
           let intersection = truncated.copy()
-          intersection.pair.push(s)
+          intersection.pair.push(street)
+
+          // append this solution
           tokenizer.solution.push(intersection)
         }
       })
