1369ce5e4b4c1444ed90c7e96a761bff2b28eaad
Edward Mac Gillavry <edward@webmapper.net>
2021-01-26 17:16:22 +0100

    Examples of correct addresses in NL.

--- test/address.nld.test.js
+++ test/address.nld.test.js
@@ -12,6 +12,38 @@ const testcase = (test, common) => {
   assert('Bosserdijk, Hoogland', [
     { street: 'Bosserdijk' }, { locality: 'Hoogland' }
   ])
+
+  assert('St Ludgerusstraat, Utrecht', [
+    { street: 'St Ludgerusstraat' }, { locality: 'Utrecht' }
+  ])
+
+  assert('Lange Groenendaal, Gouda', [
+    { street: 'Lange Groenendaal' }, { locality: 'Gouda' }
+  ])
+
+  assert('Achter Clarenburg, Utrecht', [
+    { street: 'Achter Clarenburg' }, { locality: 'Utrecht' }
+  ])
+
+  assert('Brinkstraat 87, 7512EC, Enschede', [
+    { street: 'Brinkstraat' }, { housenumber: '87' }, { postcode: '7512EC' }, { locality: 'Enschede' }
+  ])
+
+  assert('Brinkstraat 87, 7512 EC, Enschede', [
+    { street: 'Brinkstraat' }, { housenumber: '87' }, { postcode: '7512 EC' }, { locality: 'Enschede' }
+  ])
+
+  assert('Weerdsingel O.Z., Utrecht', [
+    { street: 'Weerdsingel O.Z.' }, { locality: 'Utrecht' }
+  ])
+
+  assert('Oranjelaan Westzijde 41, Puttershoek', [
+    { street: 'Oranjelaan Westzije' }, { housenumber: '41' }, { locality: 'Puttershoek' }
+  ])
+
+  assert('Middelstraat 18, De Moer', [
+    { street: 'Middelstraat' }, { housenumber: '18' }, { locality: 'De Moer' }
+  ])
 }
 
 module.exports.all = (tape, common) => {
