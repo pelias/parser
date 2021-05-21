af2254cbc42002fd452cd3aa418dfde61bc7d1ad
Julian Simioni <julian@simioni.org>
2021-05-21 08:51:13 -0700

    BREAKING CHANGE: remove unit classifier
    This PR removes the unit number parsing functionality which was causing OOM errors in some situations.
    see: https://github.com/pelias/api/issues/1530

--- test/address.aus.test.js
+++ test/address.aus.test.js
@@ -6,53 +6,53 @@ const testcase = (test, common) => {
     { region: 'NSW' }, { country: 'Australia' }
   ])
 
-  assert('Unit 12/345 Main St', [
-    { unit_type: 'Unit' }, { unit: '12' },
-    { housenumber: '345' },
-    { street: 'Main St' }
-  ])
+  // assert('Unit 12/345 Main St', [
+  // { unit_type: 'Unit' }, { unit: '12' },
+  // { housenumber: '345' },
+  // { street: 'Main St' }
+  // ])
 
-  assert('U 12 345 Main St', [
-    { unit_type: 'U' }, { unit: '12' },
-    { housenumber: '345' },
-    { street: 'Main St' }
-  ])
+  // assert('U 12 345 Main St', [
+  // { unit_type: 'U' }, { unit: '12' },
+  // { housenumber: '345' },
+  // { street: 'Main St' }
+  // ])
 
-  assert('Apartment 12/345 Main St', [
-    { unit_type: 'Apartment' }, { unit: '12' },
-    { housenumber: '345' },
-    { street: 'Main St' }
-  ])
+  // assert('Apartment 12/345 Main St', [
+  // { unit_type: 'Apartment' }, { unit: '12' },
+  // { housenumber: '345' },
+  // { street: 'Main St' }
+  // ])
 
-  assert('Apt 12/345 Main St', [
-    { unit_type: 'Apt' }, { unit: '12' },
-    { housenumber: '345' },
-    { street: 'Main St' }
-  ])
+  // assert('Apt 12/345 Main St', [
+  // { unit_type: 'Apt' }, { unit: '12' },
+  // { housenumber: '345' },
+  // { street: 'Main St' }
+  // ])
 
-  assert('Lot 12/345 Main St', [
-    { unit_type: 'Lot' }, { unit: '12' },
-    { housenumber: '345' },
-    { street: 'Main St' }
-  ])
+  // assert('Lot 12/345 Main St', [
+  // { unit_type: 'Lot' }, { unit: '12' },
+  // { housenumber: '345' },
+  // { street: 'Main St' }
+  // ])
 
-  assert('U12/345 Main St', [
-    { unit_type: 'U' }, { unit: '12' },
-    { housenumber: '345' },
-    { street: 'Main St' }
-  ])
+  // assert('U12/345 Main St', [
+  // { unit_type: 'U' }, { unit: '12' },
+  // { housenumber: '345' },
+  // { street: 'Main St' }
+  // ])
 
-  assert('Lot 12/345 Illawarra Road Marrickville NSW 2204', [
-    { unit_type: 'Lot' }, { unit: '12' }, { housenumber: '345' },
-    { street: 'Illawarra Road' }, { locality: 'Marrickville' },
-    { region: 'NSW' }, { postcode: '2204' }
-  ])
+  // assert('Lot 12/345 Illawarra Road Marrickville NSW 2204', [
+  // { unit_type: 'Lot' }, { unit: '12' }, { housenumber: '345' },
+  // { street: 'Illawarra Road' }, { locality: 'Marrickville' },
+  // { region: 'NSW' }, { postcode: '2204' }
+  // ])
 
-  assert('Lot 2, Burrows Avenue, EDMONDSON PARK, NSW, Australia', [
-    { unit_type: 'Lot' }, { unit: '2' },
-    { street: 'Burrows Avenue' }, { locality: 'EDMONDSON PARK' },
-    { region: 'NSW' }, { country: 'Australia' }
-  ])
+  // assert('Lot 2, Burrows Avenue, EDMONDSON PARK, NSW, Australia', [
+  // { unit_type: 'Lot' }, { unit: '2' },
+  // { street: 'Burrows Avenue' }, { locality: 'EDMONDSON PARK' },
+  // { region: 'NSW' }, { country: 'Australia' }
+  // ])
 }
 
 module.exports.all = (tape, common) => {
