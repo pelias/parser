a746b342405865177eca16eec020118e4a269d5b
Jan Fiołka <35661939+JanF01@users.noreply.github.com>
2023-07-25 17:18:46 +0200

    feat(poland): Added support for case prefix-place-adjective

--- classifier/scheme/street.js
+++ classifier/scheme/street.js
@@ -217,6 +217,25 @@ module.exports = [
     ]
   },
   {
+    // Aleja Wojska Polskiego
+    confidence: 0.91,
+    Class: StreetClassification,
+    scheme: [
+      {
+        is: ['StreetPrefixClassification'],
+        not: ['StreetClassification', 'IntersectionClassification']
+      },
+      {
+        is: ['PlaceClassification'],
+        not: ['StreetClassification', 'IntersectionClassification']
+      },
+      {
+        is: ['AlphaClassification', 'GivenNameClassification', 'PersonClassification'],
+        not: ['StreetClassification', 'StreetPrefixClassification']
+      }
+    ]
+  },
+  {
     // Boulevard du Général Charles De Gaulle
     confidence: 0.81,
     Class: StreetClassification,
