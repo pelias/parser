be244ec8862d0e382d269a1a0ef07538a4e70feb
Jan Fiołka <35661939+JanF01@users.noreply.github.com>
2023-08-01 15:03:05 +0200

    feat(poland): Added tests for Poland numeric streets

--- test/address.pol.test.js
+++ test/address.pol.test.js
@@ -5,6 +5,26 @@ const testcase = (test, common) => {
     { street: 'Szewska' }, { housenumber: '6' },
     { locality: 'Kraków' }
   ])
+
+  assert('11 listopada 2/4', [
+    { street: '11 listopada' }, { housenumber: '2/4' }
+  ])
+
+  assert('1 Pułku Strzelców Bytomskich 2-4', [
+    { street: '1 Pułku Strzelców Bytomskich' }, { housenumber: '2-4' }
+  ])
+
+  assert('3 Warszawskiego Pułku Strzelców Polskich 11', [
+    { street: '3 Warszawskiego Pułku Strzelców Polskich' }, { housenumber: '11' }
+  ])
+
+  assert('1 Pułku Szwoleżerów 178', [
+    { street: '1 Pułku Szwoleżerów' }, { housenumber: '178' }
+  ])
+
+  assert('11 listopada 2-4', [
+    { street: '11 listopada' }, { housenumber: '2-4' }
+  ])
 }
 
 module.exports.all = (tape, common) => {
